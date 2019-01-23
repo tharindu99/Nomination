174802e5fab284b159a9f01b59333c889db4e88a
Tharindu Madushanka Peiris <madushanka.best@gmail.com>
2019-01-24 00:49:53 +0530

    Update dataSource.js

--- src/repository/dataSource.js
+++ src/repository/dataSource.js
@@ -17,7 +17,7 @@ class DataSourceFactory {
     }
     //this.dbConnection = new Sequelize('EC_NOMINATION', 'root', 'root');
     this.dbConnection = new Sequelize('heroku_f5d01a537b411d3', 'b8dc7e945d8941', '4d3ac423' ,{
-      host: "us-cdbr-iron-east-03.cleardb.net",
+      host: 'us-cdbr-iron-east-03.cleardb.net',
     });
   }
 
